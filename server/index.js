d15cde601b937d3c543b5167a3db693413e29130
Aleksandr <mamamacriminal@yandex.ru>
2022-06-18 06:45:01 +0500

    Вывод статики на сервере

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', tableRouter);
+app.use(express.static(`${__dirname}/public`));
+app.use('/', (req, res) => {
+  res.sendFile(`${__dirname}\\public\\index.html`);
+});
 
 app.listen(PORT, () => {
   console.log('SERVER STARTED ON PORT: ', PORT);
